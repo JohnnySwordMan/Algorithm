7b7e9d12594b53ff66581f82f3ad71ac15b5dede
行知 <2277087813@qq.com>
2018-09-23 12:49:40 +0800

    数组中重复的数字

--- JZOffer/Duplicate.java
+++ JZOffer/Duplicate.java
@@ -1,22 +1,22 @@
 import java.util.HashMap;
 
 /**
- * �����⣺�������ظ�������
- * ��Ŀ��������һ������Ϊn����������������ֶ���0~n-1�ķ�Χ�ڡ�������ĳЩ�������ظ��ģ�����֪���м����������ظ��ġ�Ҳ��֪��ÿ�������ظ����Ρ�
- * ���ҳ������е�һ���ظ������֡� ���磺������볤��Ϊ7������{2,3,1,0,2,5,3}����ô��Ӧ������ǵ�һ���ظ�������2
+ * 面试题：数组中重复的数字
+ * 题目描述：在一个长度为n的数组里的所有数字都在0~n-1的范围内。数组中某些数字是重复的，但不知道有几个数字是重复的。也不知道每个数字重复几次。
+ * 请找出数组中第一个重复的数字。 例如：如果输入长度为7的数组{2,3,1,0,2,5,3}，那么对应的输出是第一个重复的数字2
  * 
- * ������ 
- * ����һ�����򣬿�������ʱ�临�Ӷ�ΪO(NlogN)
+ * 分析： 
+ * 方法一：排序，可以做到时间复杂度为O(NlogN)
  * 
- * ��������HashMap����ͷ��ʼ��������Ԫ�أ�ÿɨ��һ��Ԫ�أ���������O(1)��ʱ�����жϹ�ϣ���Ƿ��Ѿ����������֡���������ڣ�����룬������ڣ������+1��Ȼ�ٴα������飬ÿ����һ�Σ���ȥHashMap�в��Ҷ�Ӧvalueֵ�Ƿ�Ϊ1�� ʱ�临�Ӷ�ΪO(N)���ռ临�Ӷ�ΪO(N)
+ * 方法二：HashMap，从头开始遍历数组元素，每扫描一个元素，都可以用O(1)的时间来判断哈希表是否已经包含该数字。如果不存在，则加入，如果存在，则次数+1。然再次遍历数组，每遍历一次，都去HashMap中查找对应value值是否为1。 时间复杂度为O(N)，空间复杂度为O(N)
  * 
- * ���������ͷ��������ƣ����Զ���int���飬int[] flag = new int[numbers.length];��������Ԫ�أ�flag[numbers[i]]++;
+ * 方法三：和方法二类似，可以定义int数组，int[] flag = new int[numbers.length];遍历数组元素，flag[numbers[i]]++;
  * 
- * �����ģ� ��ͼ��0~n-1���뵽�����±�Ϊ0~n-1�Ķ�Ӧλ���ϣ�
- * ��ͷ��βɨ������Ԫ�أ���ɨ�赽�±�Ϊi��Ԫ��ʱ����arr[i]�����ȱȽ�arr[i]�Ƿ����i��������ڣ���ɨ����һ������������ڣ���Ƚ�arr[i]��arr[arr[i]]�Ƿ���ȣ������ȣ���˵�����ظ�Ԫ�أ���Ϊarr[i]�͵�i������arr[i]��Ԫ����ȣ�
- * �������ȣ���ѵ�i��Ԫ�غ͵�arr[i]��Ԫ�ؽ�������Ϊ������ͼ������Ԫ�����򣬰���arr[0] = 0,arr[1] = 1...arr[n-1] = n-1����
+ * 方法四： 企图将0~n-1插入到数组下标为0~n-1的对应位置上，
+ * 从头到尾扫描数组元素，当扫描到下标为i的元素时，即arr[i]，首先比较arr[i]是否等于i，如果等于，则扫描下一个，如果不等于，则比较arr[i]与arr[arr[i]]是否相等，如果相等，则说明有重复元素，因为arr[i]和第i个，第arr[i]个元素相等，
+ * 如果不相等，则把第i个元素和第arr[i]个元素交换，因为我们企图将数组元素排序，按照arr[0] = 0,arr[1] = 1...arr[n-1] = n-1排序
  * 
- * ע�⣺��Ȼ������������ѭ��������ÿ���������ֻ�жϣ�������Ҫ2�μ��ɣ������ܵ�ʱ�临�Ӷ�ΪO(N)���ռ临�Ӷ�ΪO(1)��
+ * 注意：虽然代码中有两层循环，但是每个数字最多只判断，交换需要2次即可，所以总的时间复杂度为O(N)，空间复杂度为O(1)。
  * 
  * @author geyan
  *
@@ -24,7 +24,7 @@ import java.util.HashMap;
 public class Duplicate {
 
 	/**
-	 * HashMap��������������ģ��HashMap
+	 * HashMap，或者用数组来模拟HashMap
 	 */
 	public int duplicate_1(int[] numbers) {
 		if (numbers == null || numbers.length <= 0)
@@ -46,7 +46,7 @@ public class Duplicate {
 	}
 
 	/**
-	 * ������
+	 * 方法二
 	 */
 	public int duplicate_2(int[] numbers) {
 		if (numbers == null || numbers.length <= 0)
@@ -56,14 +56,14 @@ public class Duplicate {
 				return -1;
 		}
 		for (int i = 0; i < numbers.length; i++) {
-			// numbers[i] == i����ɨ����һ������
-			// numbers[i] != i������ͨ���жϣ�����
+			// numbers[i] == i，则扫描下一个数字
+			// numbers[i] != i，我们通过判断，交换
 			while (numbers[i] != i) {
 				if (numbers[i] == numbers[numbers[i]]) {
-					// ˵�����ظ�Ԫ��
+					// 说明有重复元素
 					return numbers[i];
 				} else {
-					// ������i��Ԫ�غ͵�numbers[i]��Ԫ�أ����ǵ�Ŀ�ľ���ʹ��numbers[i] == i
+					// 交换第i个元素和第numbers[i]个元素，我们的目的就是使得numbers[i] == i
 					int temp = numbers[i];
 					numbers[i] = numbers[temp];
 					numbers[temp] = temp;
