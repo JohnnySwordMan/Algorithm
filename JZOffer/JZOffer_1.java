752fbb68419880320577c35dba1a73ba64e972a2
geyan <2277087813@qq.com>
2019-03-23 22:00:35 +0800

    第一题：二维数组的查找

--- JZOffer/JZOffer_1.java
+++ JZOffer/JZOffer_1.java
@@ -2,21 +2,22 @@
  * 二维数组的查找
  * Created by geyan on 2018/8/8.
  */
-public class Find {
+public class JZOffer_1 {
 
     /**
      * 在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
      * 请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
-     *
-     * 分析：选择右上角的元素array[0][array.length-1]与target比较，
+     * <p>
+     * 分析：
+     * 选择右上角的元素array[0][array.length-1]与target比较，
      * 如果array[0][array.length-1] < target，则比较array[1][array.length-1]、target
      * 如果array[0][array.length-1] > target，则比较array[0][array.length-2]、target
      * 如果array[0][array.length-1] == target，则存在
-     *
+     * <p>
      * 时间复杂度为O(n+m)，空间复杂度为O(1)
      */
     public boolean find(int target, int[][] array) {
-        if (array == null || array.length <= 0) {
+        if (array == null || array.length == 0) {
             return false;
         }
         int row = 0, col = array[0].length - 1;
